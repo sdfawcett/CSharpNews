7839e0b3528a8ae97594a1416d009850b2de2073
Shayne Fawcett <shayne.fawcett@gmail.com>
2023-10-04 15:37:56 -0400

    first commit

---
+++ Models/Comment.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using TheBlogProject.Enums;
+
+namespace TheBlogProject.Models
+{
+    public class Comment
+    {
+        public int? Id { get; set; }
+        public int? PostId { get; set; }
+        public string? BlogUserId { get; set; }
+        public string? ModeratorId { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Comment")]
+        public string? Body { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public DateTime? Moderated { get; set; }
+        public DateTime? Deleted { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Moderated Comment")]
+        public string? ModeratedBody { get; set; }
+
+        public ModerationType? ModerationType { get; set; }
+
+        //Navigation properties
+        public virtual Post Post { get; set; }
+        public virtual BlogUser BlogUser { get; set; }
+        public virtual BlogUser Moderator { get; set; }
+    }
+}
